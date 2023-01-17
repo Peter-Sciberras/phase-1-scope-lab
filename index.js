4640dfff48807d878f6b596b08be238dde82f4f9
Peter-Sciberras <sciberrasp@yahoo.com.au>
2023-01-17 11:39:05 +1100

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// declaring variable
+var customerName = 'bob';
+
+//Upper case customer name
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+  }
+
+  //Set Best Customer
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  
+  //Overwrite Best Customer
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+
+  //Declareing Constant
+  const leastFavoriteCustomer = 'John';
+
+  // Attempt to change the constant
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Mary';
+  }
